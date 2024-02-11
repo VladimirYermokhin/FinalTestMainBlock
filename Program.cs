d46e772abfa0cbe84169716cea5d43383b9ccc98
Vladimir Yermokhin <thankyousomuchimcrazy@gmail.com>
2024-02-11 18:59:01 +0600

    Удалил лишнее

--- Program.cs
+++ Program.cs
@@ -1,10 +1,4 @@
-﻿/* Console.InputEncoding = System.Text.Encoding.GetEncoding("utf-16");
-Console.WriteLine("Введите строки массива через пробел");
-string input = Console.ReadLine()!;
-string[] inputArray = input.Split(' ');
- */
-
-Console.InputEncoding = System.Text.Encoding.GetEncoding("utf-16");
+﻿Console.InputEncoding = System.Text.Encoding.GetEncoding("utf-16");
 Console.WriteLine("Введите строки массива через пробел");
 string input = Console.ReadLine()!;
 string[] inputArray = input.Split(' ');
