02ef7a68b67c0e1e803222321545c201a18f9fba
Vladimir Yermokhin <thankyousomuchimcrazy@gmail.com>
2024-02-11 18:52:30 +0600

    Определяем размер нового массива

--- Program.cs
+++ Program.cs
@@ -1,4 +1,20 @@
-﻿Console.InputEncoding = System.Text.Encoding.GetEncoding("utf-16");
+﻿/* Console.InputEncoding = System.Text.Encoding.GetEncoding("utf-16");
 Console.WriteLine("Введите строки массива через пробел");
 string input = Console.ReadLine()!;
 string[] inputArray = input.Split(' ');
+ */
+
+Console.InputEncoding = System.Text.Encoding.GetEncoding("utf-16");
+Console.WriteLine("Введите строки массива через пробел");
+string input = Console.ReadLine()!;
+string[] inputArray = input.Split(' ');
+
+int newSize = 0;
+for (int i = 0; i < inputArray.Length; i++)
+{
+    if (inputArray[i].Length <= 3)
+    {
+        newSize++;
+    }
+}
+
